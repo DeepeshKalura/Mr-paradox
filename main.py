917dc0a79ca0fd481852f10d67099207e60b5a46
DeepeshKalura <deepeshkalurs@gmail.com>
2024-04-05 00:25:18 +0530

    Add weather and money functions

--- main.py
+++ main.py
@@ -4,43 +4,36 @@ import time
 import argparse
 from dotenv import load_dotenv
 import requests
+import asyncio
 
 
 load_dotenv()
 
 appid = os.getenv("APPID")
 
-loc = "USA"
-# date = "2023-01-01"
+loc = "Washington"
+date = "1776-01-01"
+money = "4000"
 
+def get_weather(loc:str, date:str) -> str:
+    url = f"http://api.wolframalpha.com/v2/query?input=Weather%20{loc}%20{date}&appid={appid}&format=plaintext&includepopid=WeatherSummary:WeatherData&output=JSON"
+    response = requests.get(url)
+    data = response.json()
+    report:str = (data["queryresult"]["pods"][1]["subpods"][0]["plaintext"])
+    return report
 
-
-# url = f"http://api.wolframalpha.com/v2/query?input=Weather%20{loc}%20{date}&appid={appid}&format=plaintext&includepopid=WeatherSummary:WeatherData&output=JSON"
-# response = requests.get(url)
-# data = response.json()
-
-# report:str = (data["queryresult"]["pods"][1]["subpods"][0]["plaintext"])
-# print(report)
-# time.sleep(5)
-
-
-#  No guranatee that the data will be in the same format as the previous data ( true ) 
-
-# Wolfram Alpha API  <-- fix response nhi dati
-
-# Genai API <-- fix response deti hai
-loc = "USA"
-money = "2500" # INR 
-date_year = 1934
-money_url = f'http://api.wolframalpha.com/v2/query?input={money}+rupee+worth+in+{date_year}&appid={appid}&format=plaintext&output=JSON'
-
-response = requests.get(money_url)
-data = response.json()
-
-report:str = (data["queryresult"]["pods"][1]["subpods"][0]["plaintext"])    
-print(report)
+def get_money(money:str, date_year:int) -> str:
+    money_url = f'http://api.wolframalpha.com/v2/query?input={money}+rupee+worth+in+{date_year}&appid={appid}&format=plaintext&output=JSON'
+    response = requests.get(money_url)
+    data = response.json()
+    report:str = (data["queryresult"]["pods"][1]["subpods"][0]["plaintext"])    
+    return report
 
 
 
 
-# current money (1000 ) INR in date 
+print(get_weather(loc, date))
+print("---"*90)
+print()
+print(get_money(money, 1776))
+
