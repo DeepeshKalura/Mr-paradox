09a5e6f2d702121988acf6b528e5937732ce8ade
DeepeshKalura <deepeshkalurs@gmail.com>
2024-04-06 23:23:45 +0530

    refactor the code

--- main.py
+++ main.py
@@ -1,39 +1,19 @@
-import os
-import datetime
-import time
 import argparse
-from dotenv import load_dotenv
-import requests
-import asyncio
 
 
-load_dotenv()
+def main():
+    parser = argparse.ArgumentParser(
+                        prog='paradox',
+                        description='Mr. Paradox Travel Guide for Time Traveler',
+                        # epilog='Text at the bottom of help',
+                        )
 
-appid = os.getenv("APPID")
-
-loc = "Washington"
-date = "1776-01-01"
-money = "4000"
-
-def get_weather(loc:str, date:str) -> str:
-    url = f"http://api.wolframalpha.com/v2/query?input=Weather%20{loc}%20{date}&appid={appid}&format=plaintext&includepopid=WeatherSummary:WeatherData&output=JSON"
-    response = requests.get(url)
-    data = response.json()
-    report:str = (data["queryresult"]["pods"][1]["subpods"][0]["plaintext"])
-    return report
-
-def get_money(money:str, date_year:int) -> str:
-    money_url = f'http://api.wolframalpha.com/v2/query?input={money}+rupee+worth+in+{date_year}&appid={appid}&format=plaintext&output=JSON'
-    response = requests.get(money_url)
-    data = response.json()
-    report:str = (data["queryresult"]["pods"][1]["subpods"][0]["plaintext"])    
-    return report
+    
+    parser.add_argument('guide', help='Guide for Time Traveler')
+    args = parser.parse_args()
 
 
+   
 
-
-print(get_weather(loc, date))
-print("---"*90)
-print()
-print(get_money(money, 1776))
+main()
 
