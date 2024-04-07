df415a9e8c66ac2cf6460a65d3812217558cbfee
DeepeshKalura <deepeshkalurs@gmail.com>
2024-04-08 00:44:34 +0530

    resolve no package found error

--- app/llm_model.py
+++ app/llm_model.py
@@ -1,10 +1,12 @@
 import os
+import sys
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.prompt_template import get_prompt
 
 
 model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=1, google_api_key=os.getenv("GEMINI_API_KEY"))
 def model_response(weather:str, money:str, money_value:str, location:str, dates:str) -> str:
     prompt = get_prompt(weather, money, money_value, location, dates)
-    return model.invoke(prompt)
+    return model.invoke(prompt).content
