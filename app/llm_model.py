122c20a2125bc8094a991fb281bc9c167696e1f8
DeepeshKalura <deepeshkalurs@gmail.com>
2024-04-12 08:38:56 +0530

    Add dotenv import and load_dotenv() function call in llm_model.py

--- app/llm_model.py
+++ app/llm_model.py
@@ -1,10 +1,11 @@
 import os
 import sys
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.prompt_template import get_prompt
-
+load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=1, google_api_key=os.getenv("GEMINI_API_KEY"))
 def model_response(weather:str, money:str, money_value:str, location:str, dates:str) -> str:
